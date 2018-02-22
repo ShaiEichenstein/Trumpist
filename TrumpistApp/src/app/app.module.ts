710df4553ccddfa79b3aa80ec4f22277d86a516a
Merav <Merav@mail.com>
2018-02-22 15:47:03 +0200

    generate the tramp item component

--- TrumpistApp/src/app/app.module.ts
+++ TrumpistApp/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TrampItemComponent } from './tramp-item/tramp-item.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrampItemComponent
   ],
   imports: [
     BrowserModule
