d1230670c7721ec9ffa59c2fcf7add29f514eda1
drorifar <drorifar@gmail.com>
2018-02-22 15:26:49 +0200

    tramp dm full

--- TrumpistApp/src/app/models/tramp.ts
+++ TrumpistApp/src/app/models/tramp.ts
@@ -1,11 +1,22 @@
 export class Tramp {
-    driverFirstName: string;
-    lastName: string;
-
+    driverDetails: Driver;
+    entranceAvgTime: Date;
+    leavingAvgTime: Date;
+    trampGrade: number;
 }
 
 
 
 export class Driver {
+    driverFirstName: string;
+    driverLastName: string;
+    driverAddress: Address;
+}
 
+
+export class Address {
+    city: string;
+    naborhood: string;
+    street: string;
+    buldingNumber: number;
 }
